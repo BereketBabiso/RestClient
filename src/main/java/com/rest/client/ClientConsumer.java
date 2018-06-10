b2d2f69271d6d9ea6cf9427ec1f8cbebf7446595
BereketBabiso <berryboyy@yahoo.com>
2018-06-10 18:11:57 -0500

    restclient

---
+++ src/main/java/com/rest/client/ClientConsumer.java
@@ -0,0 +1,84 @@
+package com.rest.client;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+
+import com.rest.client.model.Product;
+
+@Controller
+public class ClientConsumer {
+	
+	private RestTemplate restTemplate = new RestTemplate();
+	
+	@RequestMapping(value="/")
+	public String homepage() {
+		
+		return "home";
+	}
+	
+	
+	@RequestMapping(value="/welcome")
+	public String welcome(Model model) {
+		System.out.println("Print");
+		String url = "http://localhost:8080/api/listProducts";
+		List<LinkedHashMap> products = restTemplate.getForObject(url, List.class);
+		//System.out.println(products.get(0).getProductName());
+		
+		model.addAttribute("products", products);
+		
+		return "listProduct";
+	}
+	@RequestMapping(value="/addProduct")
+	public String addProduct(Model model) {
+		Product product = new Product();
+		model.addAttribute("product", product);
+		
+		return "createProduct";
+	}
+	
+	@RequestMapping(value="/client/addProduct",method=RequestMethod.POST)
+	public String addProductPost(Product product) {
+		
+		System.out.println(">>>>>>>>>product creation method>>>>>>>"+product.getProductName());
+		Product pp = new Product();
+		pp.setProductName("llllll");
+		//String url = "http://localhost:8080/api/createProduct";
+		restTemplate.postForObject("http://localhost:8080/api/createProduct", pp, Product.class);
+		
+		System.out.println(">>>>>>>>>product creation method>>>>>>>"+product.getProductName());
+		
+		return "createProduct";
+	}
+
+	
+	@RequestMapping(value="/personList")
+	public String getPersons(Model model) {
+		
+		String url = "http://localhost:8080/api/listPersons";
+		List<LinkedHashMap> persons = restTemplate.getForObject(url, List.class);
+		//System.out.println(products.get(0).getProductName());
+		
+		model.addAttribute("persons", persons);
+		
+		return "listPerson";
+	}
+	
+	@RequestMapping(value="/orderList")
+	public String getOrders(Model model) {
+		System.out.println("In the Order method of the client");
+		String url = "http://localhost:8080/api/listOrders";
+		List<LinkedHashMap> orders = restTemplate.getForObject(url, List.class);
+		//System.out.println(products.get(0).getProductName());
+		
+		model.addAttribute("orders", orders);
+		
+		return "listOrder";
+	}
+
+}
